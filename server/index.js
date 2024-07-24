f5f4463b9d39d24917029454c26d489448ae7d63
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-07-24 22:19:03 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import connectDB from "./config/DB.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import usersRoutes from "./routes/usersRoutes.js";
+import tweetsRouter from "./routes/tweetRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,15 +11,17 @@ app.use(express.json());
 // Resolving dirname for ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname,"/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.use("/users/user", usersRoutes);
+app.use("/tweets", tweetsRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"/client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
 });
 
 connectDB();
 
 app.listen(5500, () => {
-    console.log(`Server is running on port 5500`);
-  });
+  console.log(`Server is running on port 5500`);
+});
