e6589cd41b4f8462739c681f389b7a4cd0f19cb5
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-08-01 17:23:23 +0300

    edit url

--- server/client/src/controllers/userController.js
+++ server/client/src/controllers/userController.js
@@ -11,7 +11,7 @@ const navigate=useNavigate()
 
 
 const handleLogin=(data)=>{
-    fetch("/users/login",{
+    fetch("/users/user/login",{
         method:"post",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify(data)
