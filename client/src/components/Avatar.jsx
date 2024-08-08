47bcff9c8e78fce8b71a2f07faa20ba05a19a517
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-08-08 08:26:56 +0300

    ""

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Avatar = ({img,userId, style}) => {
   return (
-<Link to={`/home/${userId}/profile`}>
+<Link to={userId ? `/home/${userId}/profile`:null}>
   <div className="avatar cursor-pointer">
   <div className={`w-9 h-9 rounded-full block ${style}`}>
     <img src={img|| "https://res.cloudinary.com/deqnekemi/image/upload/v1721735371/User-Profile-PNG-Clipart_gb6vgf.png"} />
